134eda3353b95f161e2a00ba29c81212661c01ae
stefano.cavazzini <digiworkscom@gmail.com>
2022-05-02 15:52:10 +0200

    Update ApiChat.php

--- src/code/ApiChat.php
+++ src/code/ApiChat.php
@@ -6,8 +6,8 @@ use code\components\Component;
 
 class ApiChat extends Component {
 
-    public function __construct($conf) {
-        parent::__construct($conf);
+    public function __construct($name, $conf) {
+        parent::__construct($name, $conf);
     }
 
 }
