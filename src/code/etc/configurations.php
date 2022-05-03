134407151ccd36f4490088c7abe200b000e00b23
stefano.cavazzini <digiworkscom@gmail.com>
2022-05-03 17:19:17 +0200

    fix

--- src/code/etc/configurations.php
+++ src/code/etc/configurations.php
@@ -2,12 +2,11 @@
 
 use code\utility\Arr;
 
-
-
 return Arr::mergeRecursive(
                 [
                     "imports" => [
-                            ['lib' => 'js/utilities/timezone.js', 'tranlsator' => ''],
-                        ],
+                        ['lib' => 'js/utilities/timezone.js', 'tranlsator' => ''],
+                        ['lib' => 'js/utilities/colormapping.js', 'tranlsator' => ''],
+                    ],
                 ],
                 require 'routes.php');
