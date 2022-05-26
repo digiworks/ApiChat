b4c86e40b70ebad84f53df6e66aae2105c4129e8
stefano.cavazzini <digiworkscom@gmail.com>
2022-05-26 16:59:27 +0200

    Update configurations.php

--- src/code/etc/configurations.php
+++ src/code/etc/configurations.php
@@ -7,7 +7,7 @@ return Arr::mergeRecursive(
                     "imports" => [
                         ['lib' => 'js/utilities/timezone.js', 'tranlsator' => ''],
                         ['lib' => 'js/utilities/colormapping.js', 'tranlsator' => ''],
-                        ['lib' => 'js/components/avatar/avatar.js', 'tranlsator' => ''],
+                        ['lib' => 'js/components/avatar/avatar.js', 'tranlsator' => 'text/babel'],
                     ],
                 ],
                 require 'routes.php');
