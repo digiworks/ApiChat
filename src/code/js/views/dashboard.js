7c9ee9a7f5ed95600bbe2d202c7ae6d4b26f4d47
stefano.cavazzini <digiworkscom@gmail.com>
2022-05-27 17:45:18 +0200

    fix

--- src/code/js/views/dashboard.js
+++ src/code/js/views/dashboard.js
@@ -1,11 +1,14 @@
 function IndexPage(props) {
 
     const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 1024,
+        maxHeight: "90vh",
+        overflow: "scroll",
+        overflowX: "hidden",
         bgcolor: 'background.paper',
         border: '2px solid #000',
         boxShadow: 24,
@@ -13,14 +16,103 @@ function IndexPage(props) {
     };
 
     const [open, setOpen] = React.useState(false);
+    const [waiting, setWaiting] = React.useState(false);
     const handleOpen = () => {
         setOpen(true);
     };
     const handleClose = () => {
         setOpen(false);
     };
+    
+    const [data, setData] = React.useState([]);
+    const [refresh, setRefresh] = React.useState(false);
+    const filters = [
+        {type: "text", id: "fltName", label:  baseApp.translations().t("name", "dashboard"), value: "", field: "Name" },
+        {type: "text", id: "fltsurname", label:  baseApp.translations().t("surname", "dashboard"), value: "", field: "surname" },
+        {type: "text", id: "fltemail", label: baseApp.translations().t("email", "dashboard"), value: "", field: "email" }
+    ];
+    
+   const labels = { 
+       "Active" : baseApp.translations().t("Active", "dashboard"),
+       "Deactivated" : baseApp.translations().t("Deactivated", "dashboard")
+    };
+    var hd = [
+        {text:  baseApp.translations().t("name", "dashboard"), numeric: 0, disablePadding: 0, field: "Name"},
+        {text:  baseApp.translations().t("surname", "dashboard"), numeric: 0, disablePadding: 0, field: "surname"},
+        {text:  baseApp.translations().t("email", "dashboard"), numeric: 0, disablePadding: 0, field: "email"},
+        {text:  baseApp.translations().t("status", "dashboard"), numeric: 0, disablePadding: 0, field: "Status"}
+    ];
+    
+    const columns = [
+        {field: "Name"},
+        {field: "surname"},
+        {field: "email"},
+        {field: (row) => { return (row.Status == 1 ? labels.Active : labels.Deactivated); }}
+    ];
+    
+    
+    const handleAgree = async () => {
+      setOpen(false);
+      if(baseApp.isWeb()){
+            setWaiting(true);
+            var result = await baseApp.fetch(props.apiGateway + "/api/user/delete",data);
+            
+            if(result.status == "success"){
+                 setWaiting(false);
+                 setRefresh(true);
+             }else{
+                 if(result.status == "error")
+                 {
+                     setWaiting(false);
+                 }
+             }
+              setRefresh(false);
+      }
+      setData([]);
+    };
+  
+    const actions = [
+       {
+           icon: 'edit',
+           tooltip: 'Edit Index',
+           text: '',
+           onClick: (event, rowData) => {
+               if(baseApp.isWeb()){
+                   event.preventDefault();
+                   /*setData(rowData);*/
+                   setWaiting(true);
+                   let path = "/formuser?id=" + rowData.Id;
+                   baseApp.redirect(path, props.apiGateway);
+               }
+           }
+       },
+       {
+           icon: 'delete',
+           tooltip: 'Delete Index',
+           text: '',
+           onClick: (event, rowData) => {
+              setData(rowData);
+              setOpen(true);
+           }
+       }
+   ];
+    
+    const createuser = (e) => {
+      e.preventDefault();
+      setWaiting(true);
+      let path = "/formuser";
+      baseApp.redirect(path, props.apiGateway);
+    };
+    
+    
     return (
             <div>
+		    <Backdrop
+		        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+		        open={waiting}
+		    >
+		        <CircularProgress color="inherit" />
+		    </Backdrop>
                 <Float>
                     <span> {baseApp.translations().t("save", "dashboard")} </span>
                 </Float >
@@ -43,6 +135,18 @@ function IndexPage(props) {
                         <Typography id="modal-modal-description" sx={{mt: 2}}>
                             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
                         </Typography>
+                        <DataGridRest
+				restUrl = {"https://192.168.178.106" + "/api/user/paginate"}
+				headers = {hd}
+				keyColumn = "Id"
+				columns = {columns}
+				actions = {actions}
+				filterFields = {filters}
+				refresh = {refresh}
+				orderByHeader = {"Name"}
+				orderDirection = {"asc"}
+				denseType = {true}
+			    />
                         <Stack pt={3} pr={1} direction="row" spacing={2} style={{display: "flex", justifyContent: "flex-end"}}>
                             <Float>
                                 <ChatButton value="Close" onClick={handleClose}/>
@@ -54,4 +158,3 @@ function IndexPage(props) {
 
             );
 }
-
--- src/code/js/views/locale/locale.js
+++ src/code/js/views/locale/locale.js
@@ -1,10 +1,24 @@
 baseApp.translations().loadResourceBundle("en", "dashboard", {
     save: "Save",
-    user_list : "User list"
+    user_list: "User list",
+    name: "Name",
+    surname: "Surname",
+    email: "Email Address",
+    description: "Description",
+    Active: "Active",
+    Deactivated: "Deactivated",
+    status: "State"
 }, true, true);
 
 baseApp.translations().loadResourceBundle("it", "dashboard", {
     save: "Salva",
-     user_list : "Lista utenti"
+    user_list: "Lista utenti",
+    name: "Nome",
+    surname: "Cognome",
+    email: "Indirizzo Email",
+    description: "Descrizione",
+    Active: "Attivo",
+    Deactivated: "Disattivo",
+    status: "Stato"
 }, true, true);
 
