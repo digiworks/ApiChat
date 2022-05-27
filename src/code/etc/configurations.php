7a3b3e21624b111a21136e2d37f1e9cf746ffea3
stefano.cavazzini <digiworkscom@gmail.com>
2022-05-27 10:21:39 +0200

    add react-spring web

--- src/code/etc/configurations.php
+++ src/code/etc/configurations.php
@@ -7,6 +7,7 @@ return Arr::mergeRecursive(
                     "imports" => [
                         ['lib' => 'js/utilities/timezone.js', 'tranlsator' => ''],
                         ['lib' => 'js/utilities/colormapping.js', 'tranlsator' => ''],
+                        ['lib' => 'js\react-spring-web\9.1.2\index.umd.js', 'tranlsator' => ''],
                         ['lib' => 'js/components/avatar/avatar.js', 'tranlsator' => 'text/babel'],
                     ],
                 ],
